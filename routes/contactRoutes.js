5d5bac3d69fe7587816a1d8620923c674ec337c4
Saurabh Raj <saurabhrajs9898@gmail.com>
2026-02-08 12:52:55 +0530

    Fix project structure: isolate backend in backend folder

--- routes/contactRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { createContact } from "../controllers/contactController.js";
-
-const router = express.Router();
-
-router.post("/", createContact);
-export default router;
